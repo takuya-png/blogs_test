29338a729c4d6d4954e794aa21848bdc6136979b
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-04-13 11:05:54 +0900

    課題提出

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,52 @@
+class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  def index
+    @blogs = Blog.all
+    title1 = @blogs[0].title
+    content1 = @blogs[0].content
+  end
+  def new
+    @blog = Blog.new
+  end
+  def create
+    @blog = Blog.new(blog_params)
+    if params[:back]
+      render :new
+    else
+      if @blog.save
+        redirect_to blogs_path, notice: "ブログを作成しました！"
+      else
+        render :new
+      end
+    end
+  end
+  def show
+    @blog = Blog.find(params[:id])
+  end
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+  def update
+    @blog= Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice:"ブログを削除しました！"
+  end
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+  def confirm
+    @blog = Blog.new(blog_params)
+    render :new if @blog.invalid?
+  end
+end
